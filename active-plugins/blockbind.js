ebf8a952567b83180ce696f5f459dc0b7f3470c4
Arkadiusz Kolodziejski <a.kolodziejski@nchain.com>
2022-05-26 12:16:17 +0000

    Remove extensions support.

--- active-plugins/blockbind.js
+++
@@ -1,1 +0,0 @@
-../plugins/blockbind.js
--- services/extensions.js
+++ services/extensions.js
@@ -1,26 +1,3 @@
-const fs = require('fs')
-const path = require('path')
-
-const pluginPath = 'active-plugins'
-
-function addExtensions (doc, coinbase2, jobData = {}) {
-  const pluginFiles = fs.readdirSync(pluginPath)
-  const extensions = {}
-
-  jobData.coinbase2 = coinbase2
-
-  // run each extension plugin in order to add data to the extensions object
-  // if the data needed for that extension is provided in jobData
-  pluginFiles.forEach(pluginFile => {
-    const pluginFilePath = path.join(__dirname, '../', pluginPath, pluginFile)
-    require(pluginFilePath)({ extensions, jobData })
-  })
-
-  if (Object.keys(extensions).length !== 0) {
-    doc.extensions = extensions
-  }
-}
-
 // placeholderCB1 is the zeroed out initial part of the coinbase shown in the blockbind BRFC
 // see: https://github.com/bitcoin-sv-specs/brfc-minerid/tree/master/extensions/blockbind
 //
@@ -33,6 +10,5 @@ function addExtensions (doc, coinbase2, jobData = {}) {
 const placeholderCB1 = '01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff080000000000000000'
 
 module.exports = {
-  addExtensions,
   placeholderCB1
 }
